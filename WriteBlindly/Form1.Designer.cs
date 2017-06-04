3348b3231fbb6ad179a2de0e0747ab52148cbac6
WelloSoft <wildanmubarok22@gmail.com>
2017-06-05 06:18:21 +0700

    FIrst upload

---
+++ WriteBlindly/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WriteBlindly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this._words = new System.Windows.Forms.ToolStripStatusLabel();
+            this._lines = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.sendToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._sendClipboard = new System.Windows.Forms.ToolStripMenuItem();
+            this._sendFile = new System.Windows.Forms.ToolStripMenuItem();
+            this._selectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this._mask = new System.Windows.Forms.ToolStripMenuItem();
+            this._txt = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._words,
+            this._lines,
+            this.toolStripDropDownButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // _words
+            // 
+            this._words.Name = "_words";
+            this._words.Size = new System.Drawing.Size(48, 17);
+            this._words.Text = "Word: 0";
+            // 
+            // _lines
+            // 
+            this._lines.Name = "_lines";
+            this._lines.Size = new System.Drawing.Size(46, 17);
+            this._lines.Text = "Lines: 0";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sendToToolStripMenuItem,
+            this._selectAll,
+            this._mask});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 20);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // sendToToolStripMenuItem
+            // 
+            this.sendToToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._sendClipboard,
+            this._sendFile});
+            this.sendToToolStripMenuItem.Name = "sendToToolStripMenuItem";
+            this.sendToToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.sendToToolStripMenuItem.Text = "Send to";
+            // 
+            // _sendClipboard
+            // 
+            this._sendClipboard.Name = "_sendClipboard";
+            this._sendClipboard.Size = new System.Drawing.Size(132, 22);
+            this._sendClipboard.Text = "Clipboard";
+            // 
+            // _sendFile
+            // 
+            this._sendFile.Name = "_sendFile";
+            this._sendFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this._sendFile.Size = new System.Drawing.Size(132, 22);
+            this._sendFile.Text = "File";
+            // 
+            // _selectAll
+            // 
+            this._selectAll.Name = "_selectAll";
+            this._selectAll.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this._selectAll.Size = new System.Drawing.Size(164, 22);
+            this._selectAll.Text = "Select All";
+            this._selectAll.Click += new System.EventHandler(this._selectAll_Click);
+            // 
+            // _mask
+            // 
+            this._mask.Checked = true;
+            this._mask.CheckState = System.Windows.Forms.CheckState.Checked;
+            this._mask.Name = "_mask";
+            this._mask.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.M)));
+            this._mask.Size = new System.Drawing.Size(164, 22);
+            this._mask.Text = "Mask";
+            this._mask.Click += new System.EventHandler(this._mask_Click);
+            // 
+            // _txt
+            // 
+            this._txt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._txt.Font = new System.Drawing.Font("Consolas", 10F);
+            this._txt.Location = new System.Drawing.Point(0, 0);
+            this._txt.Multiline = true;
+            this._txt.Name = "_txt";
+            this._txt.PasswordChar = '●';
+            this._txt.Size = new System.Drawing.Size(284, 239);
+            this._txt.TabIndex = 1;
+            this._txt.TextChanged += new System.EventHandler(this._txt_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this._txt);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Write blindly";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel _words;
+        private System.Windows.Forms.ToolStripStatusLabel _lines;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem sendToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _sendClipboard;
+        private System.Windows.Forms.ToolStripMenuItem _sendFile;
+        private System.Windows.Forms.ToolStripMenuItem _mask;
+        private System.Windows.Forms.TextBox _txt;
+        private System.Windows.Forms.ToolStripMenuItem _selectAll;
+    }
+}
+
---
+++ WriteBlindly/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WriteBlindly
+{
+    public partial class Form1 : Form
+    {
+        string text;
+
+        void DrawStats()
+        {
+            var words = Regex.Matches(text, @"[\S]+");
+            var lines = text.Count(x => x == '\n');
+            _words.Text = string.Format("Words: {0}", words.Count);
+            _lines.Text = string.Format("Lines: {0}", lines + 1);
+        }
+
+        void Upload()
+        {
+            var r = GetRegPath();
+            r.SetValue("LastFile", text);
+        }
+
+        void Download()
+        {
+            var r = GetRegPath();
+            text = _txt.Text = (string)r.GetValue("LastFile", "");
+        }
+
+
+        RegistryKey GetRegPath()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+
+            key.CreateSubKey("DeadlineCounter");
+            return key.OpenSubKey("DeadlineCounter", true);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Download();
+        }
+
+        private void _txt_TextChanged(object sender, EventArgs e)
+        {
+            text = _txt.Text;
+            DrawStats();
+            Upload();
+        }
+
+        private void _mask_Click(object sender, EventArgs e)
+        {
+            _mask.Checked = !_mask.Checked;
+            _txt.UseSystemPasswordChar = !_mask.Checked;
+        }
+
+        private void _selectAll_Click(object sender, EventArgs e)
+        {
+            _txt.SelectAll();
+        }
+    }
+}
